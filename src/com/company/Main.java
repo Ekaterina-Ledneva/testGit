9eccd596aa5736ace3d324bd8169af62c713c77d
Ekaterina-Ledneva <meikberri@gmail.com>
2020-06-18 21:21:02 +0300

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");// write your code here
+        System.out.println("HelloWorld!");
+        System.out.println("Second commit");// write your code here
     }
 }
